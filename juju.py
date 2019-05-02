65202abb29fb5c637f018841eac6f154bfee7a9c
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2019-05-02 11:53:02 +0530

    upload to seedr

--- juju.py
+++ juju.py
@@ -27,3 +27,48 @@ for i in links:
     print(count, i.text)
     count = count + 1
 torrentIndex = int(input("Select a torrent : ")) - 1
+torrentLink = links[torrentIndex]['href']
+uploadResponse = requests.post(
+   'https://www.seedr.cc/oauth_test/resource.php',
+    data = {'torrent_magnet':torrentLink,'func':'add_torrent','access_token':'673446456f8ebc3f9196d87d18643936a372a339'}
+)
+user_torrent_id = uploadResponse.json()['user_torrent_id']
+loading = True
+while loading:
+    foldersResponse = requests.post(
+        'https://www.seedr.cc/api/folders',
+        data={'access_token':'673446456f8ebc3f9196d87d18643936a372a339'}
+    )
+    torrents = foldersResponse.json()['torrents']
+    loading = False
+    for torrent in torrents:
+        if torrent['id'] == user_torrent_id:
+            print("loading ",torrent['progress']," %")
+            loading = True
+print("finished")
+folders = requests.post(
+        'https://www.seedr.cc/api/folders',
+        data={'access_token':'673446456f8ebc3f9196d87d18643936a372a339'}
+    ).json()['folders']
+count = 1
+for folder in folders:
+    print(count,folder['name'])
+folderIndex = int(input("Enter a folder number to download: ")) - 1
+folder = folders[folderIndex]
+archive_arr = [{"type":"folder","id":folder['id']}]
+create_empty_archive_response = requests.post(
+    'https://www.seedr.cc/oauth_test/resource.php',
+    data = {'access_token':'673446456f8ebc3f9196d87d18643936a372a339','archive_arr':archive_arr,'func':'create_empty_archive'}
+)
+archive_id = create_empty_archive_response.json()['archive_id']
+print(create_empty_archive_response.json())
+fetch_archive_response = requests.post(
+    'https://www.seedr.cc/oauth_test/resource.php',
+    data = {'access_token':'673446456f8ebc3f9196d87d18643936a372a339','archive_id':archive_id,'func':'fetch_archive'}
+)
+print(fetch_archive_response.json())
+# requests.post(
+#     'https://www.seedr.cc/oauth_test/resource.php',
+#     data = {'access_token':'673446456f8ebc3f9196d87d18643936a372a339','delete_arr':archive_arr,'func':'delete'}
+# )
+# print(url) 
